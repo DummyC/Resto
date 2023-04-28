a19017b71bf1eb1ba6db0f4e89732ce992ae27d2
DummyC <modestopellerin@gmail.com>
2023-04-28 19:06:24 +0800

    add ascii art, system loop

--- src/App.java
+++ src/App.java
@@ -23,13 +23,28 @@ public class App {
 
     static void getName() {
         Scanner scan = new Scanner(System.in);
+        showTitle();
         System.out.print("Please enter your name: ");
         customerName = scan.nextLine();
         listMenu();
     }
 
+    static void showTitle() {
+
+        System.out.println("\u001B[35m");
+        System.out.println("  _____                                  _       _____           _        ");
+        System.out.println(" |  __ \\                                ( )     |  __ \\         | |       ");
+        System.out.println(" | |  | |_   _ _ __ ___  _ __ ___  _   _|/ ___  | |__) |___  ___| |_ ___  ");
+        System.out.println(" | |  | | | | | '_ ` _ \\| '_ ` _ \\| | | | / __| |  _  // _ \\/ __| __/ _ \\ ");
+        System.out.println(" | |__| | |_| | | | | | | | | | | | |_| | \\__ \\ | | \\ \\  __/\\__ \\ || (_) |");
+        System.out.println(" |_____/ \\__,_|_| |_| |_|_| |_| |_|\\__, | |___/ |_|  \\_\\___||___/\\__\\___/ ");
+        System.out.println("                                    __/ |                                 ");
+        System.out.println("                                   |___/                                  ");
+        System.out.println("\u001B[37m");
+    }
     static void listMenu() {
         clearScreen();
+        showTitle();
         System.out.println("Welcome, " + customerName);
         System.out.println("Select your desired items in our menu below\n");
 
@@ -93,6 +108,7 @@ public class App {
     static void checkOut() {
         Scanner scan = new Scanner(System.in);
         clearScreen();
+        showTitle();
         System.out.println("Checkout Page");
         listCart();
         System.out.println("\nAvailable Payment Methods:");
@@ -112,6 +128,22 @@ public class App {
         }
 
         System.out.println("\nThank you for your order.\nPlease wait until your order is ready");
+
+        threadSleep(3000);
+        priceTotal = 0;
+        for(int i = 0; i < 10; i++){
+            orderList[i] = 0;
+        }
+        
+        System.out.print("\nReturning to main menu in: ");
+        for(int i = 5; i > 0; i--){
+            System.out.print(i);
+            threadSleep(1000);
+            System.out.print("\033[1D\033[0K");
+        }
+
+        clearScreen();
+        getName();
     }
 
     static void checkOutGCash() {
@@ -133,6 +165,7 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
+        clearScreen();
         getName();
     }
 }
